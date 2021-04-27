80e28bd9bc017d5dcada7069dc024745bc44d3ea
safirex <eudes.chatin@gmail.com>
2021-04-27 12:36:23 +0200

    add scripts for object slow motion

---
+++ Assets/scripts/PausingGame.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Works like a charm
+public class PausingGame : MonoBehaviour
+{
+    public enum State
+    {
+        pause,
+        playing
+    };
+
+    State gameState = State.playing;
+    float timeScaleBeforePause;
+
+    public State getGameState()
+    {
+        return gameState;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timeScale!=0 basically
+        if (Input.GetKeyDown(KeyCode.Escape)
+            && gameState == State.playing)
+        {
+            gameState = State.pause;
+            timeScaleBeforePause = Time.timeScale;
+            Time.timeScale = 0;
+            //display your pause GUI
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Escape)
+             && gameState == State.pause)
+        {
+            gameState = State.playing;
+            Time.timeScale = timeScaleBeforePause;
+        }
+    }
+}
---
+++ Assets/scripts/TimeScaledObjects.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.scripts
+{
+    public class TimeScaledObjects : MonoBehaviour
+    {
+
+
+        #region time scale
+        
+
+
+        public enum Status
+        {
+            Normal,
+            Slowed
+        };
+        public Status status = Status.Normal;
+        [Range(0f, 100f)]
+        public float slowedTimeScale = 50f;
+        int skippedFrames = 1;
+
+        // Update is called once per frame
+        void Update()
+        { 
+            if(status == Status.Normal)
+            {
+                play();
+            }
+            else if (status == Status.Slowed)
+            {
+                float decimalPercent = slowedTimeScale / 100;
+                Debug.Log($" f {decimalPercent}, count {skippedFrames}");
+                if(skippedFrames * decimalPercent >= 1)
+                {
+                    skippedFrames = 1;
+                    play();
+                }
+                else
+                {
+                    skippedFrames++;
+                }
+            }
+            
+        }
+        #endregion
+
+        int accelaration = 1;
+        Vector3 deplacement = new Vector3(0, 0, 1);
+        private void play()
+        {
+            //usual comportement
+            transform.Translate(deplacement * accelaration * Time.deltaTime);
+            if (accelaration < 30 && Time.frameCount % 30 == 0)
+                accelaration++;
+        }
+
+        public void setStatus(Status state)
+        {
+            status = state;
+        }
+    }
+}
